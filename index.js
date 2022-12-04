74c6caef1636ab51d70a03c5e2583a81e7c76e9f
Nkhanal2002 <narayankhanal435@gmail.com>
2022-12-03 23:35:22 -0500

    My Simple Portfolio Website

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-hamburger = document.querySelector('.hamburger');
-navMenu = document.querySelector('.nav-menu');
-navLogo = document.querySelector('.nav-logo');
-navListMenu = document.querySelector('.navlist-menu');
+'use strict'
+// creating constant varibles for different class names
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const navLogo = document.querySelector('.nav-logo');
+const navListMenu = document.querySelector('.navlist-menu');
 
+// adding addEventListener to toggle between different classes
 hamburger.addEventListener('click',()=>{
     navListMenu.classList.toggle('v-resp-navmenu');
     navLogo.classList.toggle('v-resp-navmenu');
